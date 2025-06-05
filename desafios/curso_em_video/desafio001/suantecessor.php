c9e48edca36fa1083333a24fc33c11713940297d
Erick Gabriel <erickgabrie56@gmail.com>
2025-06-05 19:49:49 -0300

    05-06-2025 19:49

--- desafios/curso_em_video/desafio001/suantecessor.php
+++ desafios/curso_em_video/desafio001/suantecessor.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,11 +11,15 @@
         <h1>Resultado Final</h1>
         <p>
             <?php
-                $numero = $_POST['numero'] ?? 0;
-                echo "O número escolhido foi <strong>$numero</strong> <br>";
-                echo "O seu <em>antecessor</em> é " . ($numero - 1);
-                echo "<br>";
-                echo "O seu <em>sucessor</em> é " . ($numero + 1);
+                if (!isset($_POST['numero']) || $_POST['numero'] === ''){
+                    echo 'Por favor digite um número!';
+                } else{
+                    $numero = $_POST['numero'];
+                    echo "O número escolhido foi <strong>$numero</strong> <br>";
+                    echo "O seu <em>antecessor</em> é " . ($numero - 1);
+                    echo "<br>";
+                    echo "O seu <em>sucessor</em> é " . ($numero + 1);
+                }
             ?>
         </p>
         <button onclick="javascript:history.go(-1)">&#x2B05; Voltar</button>
--- desafios/curso_em_video/desafio002/index.php
+++ desafios/curso_em_video/desafio002/index.php
@@ -4,8 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Numeros Aleatorios</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action=""></form>
+    <main>
+        <section>
+            <h1>Trabalhando com números aleatórios</h1>
+            
+            <?php
+                $min = 0;
+                $max = 100;
+                $aleatorio = mt_rand($min, $max);
+                echo 'Gerando um número aleatório entre 0 e 100...<br>';
+                echo "O valor gerado foi $aleatorio";
+            ?>
+            <br> <button onclick="javascript:document.location.reload()">&#x1F504; Gerar</button> 
+            
+        </section>
+    </main>
+    
 </body>
 </html>
