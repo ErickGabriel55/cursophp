f985505bd5daae9212da72fce4c76b9ee70b1a7a
Erick Gabriel <erickgabrie56@gmail.com>
2026-04-16 12:41:16 -0300

    feat: adiciona a classe funcionario e inicia estudos de POO

---
+++ desenvolvimento_web_udemy/secao21_phpoo/ex388_abstracao.php
@@ -0,0 +1,11 @@
+<?php
+    // modelo (primeira letra de cada palavra sempre em caixa alta)
+    class Funcionario {
+        // atributos
+        public $nome = null;
+        public $telefone = null;
+        public $numFilhos = null;
+
+        // métodos
+    }
+?>
